639595766044395822842f6a7b78fc5bfa10ae44
Denis Chobanov <denischobanov@Deniss-MacBook-Pro.local>
2026-06-14 16:04:31 +0300

    Add timestamp to the report file name

--- python/log_analyzer/log_analyzer.py
+++ python/log_analyzer/log_analyzer.py
@@ -1,26 +1,29 @@
 import os
 from datetime import datetime
-timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+timestamp=datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 path = input("Enter log file path: ")
-reportPath = os.path.join(os.path.dirname(path),'analysis_report.txt')
+reportPath = os.path.join(os.path.dirname(path),f'analysis_report_{timestamp}.txt')
 errCnt = 0
 wrnCnt = 0
 infoCnt = 0
+totalEntries =0
 
 try:
     with open(path,'r') as logFile:
         for line in logFile:
             if 'error' in line.lower():
                 errCnt+=1
-            elif 'warning' in line.lower():
+            if 'warning' in line.lower():
                 wrnCnt+=1
-            elif 'info' in line.lower():
+            if 'info' in line.lower():
                 infoCnt+=1
-    with open(reportPath,'a') as reportFile:
-        reportFile.write(f'===== LOG ANALYSIS REPORT ({timestamp}) =====\n\n\n')
+            totalEntries+=1
+    with open(reportPath,'w') as reportFile:
+        reportFile.write(f'===== LOG ANALYSIS REPORT =====\n\n\n')
         reportFile.write(f'Errors: {errCnt}\n\n')
         reportFile.write(f'Warnings: {wrnCnt}\n\n')
         reportFile.write(f'Info: {infoCnt}\n\n')
-        
+        reportFile.write(f'Total entries: {totalEntries}\n\n')
+    print(f'Report saved to {reportPath}')
 except FileNotFoundError:
     print('Invalid path! Make sure you write the correct path!')
