954fde71d1b47ab2bc5a73cab4afb9622e44a087
Denis Chobanov <denischobanov@Deniss-MacBook-Pro.local>
2026-06-14 17:40:12 +0300

    Add recurring error detection and top issue analysis

--- python/log_analyzer/log_analyzer.py
+++ python/log_analyzer/log_analyzer.py
@@ -1,29 +1,50 @@
 import os
 from datetime import datetime
+
 timestamp=datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
 path = input("Enter log file path: ")
 reportPath = os.path.join(os.path.dirname(path),f'analysis_report_{timestamp}.txt')
+
 errCnt = 0
 wrnCnt = 0
 infoCnt = 0
 totalEntries =0
 
+errDict = {}
+maxValue=-1
+topError='None'
+
 try:
     with open(path,'r') as logFile:
         for line in logFile:
             if 'error' in line.lower():
+                parts=line.split(':',1)
+                errMessage=parts[1].strip()
+                if errMessage not in errDict:
+                    errDict[errMessage] = 1
+                else:
+                    errDict[errMessage]+=1
                 errCnt+=1
             if 'warning' in line.lower():
                 wrnCnt+=1
             if 'info' in line.lower():
                 infoCnt+=1
             totalEntries+=1
+    
     with open(reportPath,'w') as reportFile:
         reportFile.write(f'===== LOG ANALYSIS REPORT =====\n\n\n')
         reportFile.write(f'Errors: {errCnt}\n\n')
         reportFile.write(f'Warnings: {wrnCnt}\n\n')
         reportFile.write(f'Info: {infoCnt}\n\n')
         reportFile.write(f'Total entries: {totalEntries}\n\n')
+        reportFile.write(f'===== MOST COMMON ERRORS =====\n\n\n')
+        for key,value in errDict.items():
+            reportFile.write(f'{key}: {value}\n\n')
+            if value>maxValue:
+                maxValue= value
+                topError=key
+        reportFile.write(f'\nTop Issue: {topError} (Occurrences: {maxValue})')
     print(f'Report saved to {reportPath}')
 except FileNotFoundError:
     print('Invalid path! Make sure you write the correct path!')
