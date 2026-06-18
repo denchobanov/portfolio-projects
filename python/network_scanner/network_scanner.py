e6d6b929c69b80bc70cd3f187d7fb4a5f187b138
Denis Chobanov <denischobanov@Deniss-MacBook-Pro.local>
2026-06-18 15:01:57 +0300

    Refactor scanner using functions

--- python/network_scanner/network_scanner.py
+++ python/network_scanner/network_scanner.py
@@ -1,19 +1,33 @@
 import socket, subprocess
 
-ip = input("Enter IP Address: ")
-checkOnline = subprocess.run(['ping', '-c', '4', ip])
-if checkOnline.returncode != 0:
-    print("The host is unreachable!")
+def ping_host(ip):
+    checkOnline = subprocess.run(['ping', '-c', '4', ip])
+    if checkOnline.returncode != 0:
+       return False
+    return True
+
+def scan_ports(ip, ports):
+    scan_results = {}
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        connection_status = sock.connect_ex((ip,port))
+        if connection_status == 0:
+            scan_results[port] = 'OPEN'
+        else: 
+            scan_results[port] = 'CLOSED'
+        sock.close()
+    return scan_results
+
+def print_results(results):
+    print("\n===== SCAN RESULTS =====\n")
+    for port, status in results.items():
+        print(f'PORT {port}: {status}')
+
+ip = input('Enter an ip: ')
+if not ping_host(ip):
+    print('Host unreachable!')
     exit()
-
 ports = [22, 80, 443, 3389]
-
-for port in ports:
-    scket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    scket.settimeout(1)
-    result = scket.connect_ex((ip,port))
-    if result == 0:
-        print(f'PORT {port}: OPEN')
-    else: 
-        print(f'PORT {port}: CLOSED')
-    scket.close()
+scan_results = scan_ports(ip, ports)
+print_results(scan_results)
